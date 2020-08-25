bc17313b89d3aa3bab5990a77f2ff61e07f3ae19
Jefwillems <jefwillems100@gmail.com>
2020-08-25 14:10:56 +0200

    add turtle support

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { promises } from 'fs';
+import $rdf from 'rdflib';
 import getData from './fetch.js';
 import { extractRows } from './row.js';
 import { mapToRdf } from './rml/index.js';
-import { DEBUG } from './rml/config.js';
 
 const app = express();
 
-app.get('/score.rdf', (req, res) => {
+app.get('/score', (req, res, next) => {
+  const format = req.query.format || 'json';
   const data = getData(
     'https://spreadsheets.google.com/feeds/cells/1vBJ9rB7NttdM98DbF0cBm6C1QD5nVmom53iisk3mtTc/1/public/full?alt=json',
   ).catch((err) => console.log(err));
@@ -15,15 +15,31 @@ app.get('/score.rdf', (req, res) => {
   data.then(({ feed: { entry } }) => {
     const filtered = entry.filter((e) => e.gs$cell.col <= 7);
     const rows = extractRows(filtered);
-    return mapToRdf(JSON.stringify(rows));
+    return mapToRdf(JSON.stringify(rows), format === 'json');
   }).then((rdf) => {
-    if (!DEBUG) {
-      res.setHeader('Content-type', 'application/rdf+xml');
-      res.set({ 'Content-Disposition': 'attachment; filename="score.rdf"' });
-      return res.send(rdf);
+    switch (format) {
+      case 'json':
+        res.json(rdf);
+        break;
+      case 'ttl':
+        // eslint-disable-next-line no-case-declarations
+        const store = new $rdf.Formula();
+        $rdf.parse(rdf, store, 'http://example.org', 'text/n3');
+
+        $rdf.serialize(undefined, store, 'http://example.org', 'text/turtle', (err, triples) => {
+          if (err) { console.error(err); }
+          res.setHeader('Content-type', 'text/turtle');
+          res.send(triples);
+        });
+        break;
+      case 'rdf':
+        res.setHeader('Content-type', 'application/rdf+xml');
+        res.set({ 'Content-Disposition': 'attachment; filename="score.rdf"' });
+        res.send(rdf);
+        break;
+      default:
+        next(new Error(`format "${format}" is not valid, use "json"/"rdf"/"ttl"`));
     }
-    promises.writeFile('./score.rdf', rdf, { encoding: 'utf-8' });
-    return res.json({ created: 1 });
   });
 });
 
--- src/rml/index.js
+++ src/rml/index.js
@@ -1,13 +1,16 @@
 import { promises } from 'fs';
 import rml from 'rocketrml';
+
 import { RML_FILE, RML_OPTIONS } from './config.js';
 
 const readFileAsync = promises.readFile;
 
-export const mapToRdf = async (jsonstring) => {
+export const mapToRdf = async (jsonstring, returnJson) => {
   const fileContents = await readFileAsync(RML_FILE, { encoding: 'utf-8' });
   if (fileContents) {
-    return rml.parseFileLive(fileContents, { input: jsonstring }, RML_OPTIONS)
+    return rml.parseFileLive(fileContents,
+      { input: jsonstring },
+      { ...RML_OPTIONS, toRDF: !returnJson })
       .catch((err) => console.error(err));
   }
   return null;
